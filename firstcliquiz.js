aef66477a85338ac8d744fbb15aa7c83172c25e6
Rohini Bali <rohinibalik@gmail.com>
2020-11-30 16:14:47 +0530

    feat-function play added

--- firstcliquiz.js
+++ firstcliquiz.js
@@ -5,3 +5,17 @@ var highScore={name:"Rohini",hscore:07};
 console.log(chalk.cyan('Hello Players!'));
 console.log(chalk.magentaBright("The high scorer is "+ highScore.name + " and the highscore is "+highScore.hscore));
 console.log(chalk.yellowBright("Let's see how far can you go?"));
+function play(question,answer)
+{
+ var userAnswer=readlineSync.question(question);
+ if(userAnswer===answer)
+ {
+   console.log(chalk.green("Great job!Your answer is Right!"));
+   score=++score;
+ }
+ else
+ {
+   console.log(chalk.red("Oops! You're Wrong!"));
+   score=--score;
+ }
+}
